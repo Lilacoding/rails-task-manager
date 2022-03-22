1e7e0a0c8680a4f94142a10bce121eddd7e5ac46
Lila Atmani <sunbornone@gmail.com>
2022-03-22 15:29:15 +0100

    index and show ok

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,9 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
   end
 end
